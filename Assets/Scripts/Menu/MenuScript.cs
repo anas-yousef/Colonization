60f77a440418da46f2ee7197cde807ac56627212
anas-yousef <anas.malik.yousef@gmail.com>
2020-12-13 23:52:54 +0200

    Added stuff

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public Button previousButton;
+    public Button nextButton;
+    public Text explanation0;
+    public Text explanation1;
+    public Text explanation2;
+    private int explanationNum;
+    
+    public void Start()
+    {
+        explanationNum = 0;
+    }
+
+    // Starting the game.
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Quit the game.
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Switching to the next brief explanation.
+    public void ToNextExplanation()
+    {
+        if (explanationNum == 0)
+        {
+            explanationNum = 1;
+            explanation0.gameObject.SetActive(false);
+            explanation1.gameObject.SetActive(true);
+            previousButton.gameObject.SetActive(true);
+            return;
+        }
+        if (explanationNum == 1)
+        {
+            explanationNum = 2;
+            explanation1.gameObject.SetActive(false);
+            explanation2.gameObject.SetActive(true);
+            nextButton.gameObject.SetActive(false);
+            return;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    // Switching to the next brief explanation.
+    public void ToPreviousExplanation()
+    {
+        if (explanationNum == 1)
+        {
+            explanationNum = 0;
+            explanation1.gameObject.SetActive(false);
+            explanation0.gameObject.SetActive(true);
+            previousButton.gameObject.SetActive(false);
+            return;
+        }
+        if (explanationNum == 2)
+        {
+            explanationNum = 1;
+            explanation2.gameObject.SetActive(false);
+            explanation1.gameObject.SetActive(true);
+            nextButton.gameObject.SetActive(true);
+            return;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
--- Assets/Scripts/Objects/Asteroid/AsteroidsScript.cs
+++ Assets/Scripts/Objects/Asteroid/AsteroidsScript.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class AsteroidsScript : MonoBehaviour
 {
@@ -23,78 +23,84 @@ public class AsteroidsScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //asteroidsNum = 4;
-        //asteroidsDistance = CanvasDimensions.canvasHeight / asteroidsNum;
-        //layersNum = 3;
-        //layersWidth = 0.125f * CanvasDimensions.canvasWidth;
-        //layersDistance = layersWidth / (layersNum - 1);
-        //asteroidsLinearSpeed = new float[3];
-        //asteroidAngularSpeed = new float[(int)layersNum, (int)(asteroidsNum + 1)];
-        //initialYpos = -(asteroidsDistance + CanvasDimensions.canvasHeight) / 2;
-        //scale = 0.15f;
-        //asteroidSprite0 = Resources.Load("_steroid1", typeof(Sprite)) as Sprite;
-        //asteroidSprite1 = Resources.Load("_steroid2", typeof(Sprite)) as Sprite;
-        //asteroidSprite2 = Resources.Load("_steroid3", typeof(Sprite)) as Sprite;
-        //asteroidsSprites = new Sprite[] { asteroidSprite0, asteroidSprite1, asteroidSprite2 };
+        asteroidsNum = 4;
+        asteroidsDistance = CanvasDimensions.canvasHeight / asteroidsNum;
+        layersNum = 3;
+        layersWidth = 0.125f * CanvasDimensions.canvasWidth;
+        layersDistance = layersWidth / (layersNum - 1);
+        asteroidsLinearSpeed = new float[3];
+        asteroidAngularSpeed = new float[(int)layersNum, (int)(asteroidsNum + 1)];
+        initialYpos = -(asteroidsDistance + CanvasDimensions.canvasHeight) / 2;
+        scale = 0.15f;
+        asteroidSprite0 = Resources.Load("_steroid1", typeof(Sprite)) as Sprite;
+        asteroidSprite1 = Resources.Load("_steroid2", typeof(Sprite)) as Sprite;
+        asteroidSprite2 = Resources.Load("_steroid3", typeof(Sprite)) as Sprite;
+        asteroidsSprites = new Sprite[] { asteroidSprite0, asteroidSprite1, asteroidSprite2 };
 
-        //// Creating a pool of asteroids.
-        //for (int xp = 0; xp < layersNum; ++xp)
-        //{
-        //    float xPos = -layersWidth / 2 + xp * layersDistance;
-        //    asteroidsLinearSpeed[xp] = Mathf.Pow(-1, xp) * 2f / (Mathf.Abs(xPos) + 1);
-        //    for (int a = 0; a < asteroidsNum + 1; ++a)
-        //    {
-        //        // Instantiating.
-        //        GameObject asteroidObj = Instantiate(Resources.Load("Asteroid")) as GameObject;
-        //        asteroidObj.GetComponent<SpriteRenderer>().sprite = asteroidsSprites[(xp + a) % 3];
-        //        // Positioning.
-        //        float yPos = initialYpos + a * asteroidsDistance;
-        //        asteroidObj.transform.position = new Vector3(xPos, yPos, 0f);
-        //        // Scaling.
-        //        asteroidObj.transform.localScale = new Vector3(scale, scale, 1);
-        //        asteroidAngularSpeed[xp, a] = Mathf.Pow(-1, a) * Random.Range(25f, 40f);
-        //    }
-        //}
-        //asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
+        // Creating a pool of asteroids.
+        for (int xp = 0; xp < layersNum; ++xp)
+        {
+            float xPos = -layersWidth / 2 + xp * layersDistance;
+            asteroidsLinearSpeed[xp] = Mathf.Pow(-1, xp) * 2f / (Mathf.Abs(xPos) + 1);
+            for (int a = 0; a < asteroidsNum + 1; ++a)
+            {
+                // Instantiating.
+                GameObject asteroidObj = Instantiate(Resources.Load("Asteroid")) as GameObject;
+                asteroidObj.GetComponent<SpriteRenderer>().sprite = asteroidsSprites[(xp + a) % 3];
+                // Positioning.
+                float yPos = initialYpos + a * asteroidsDistance;
+                asteroidObj.transform.position = new Vector3(xPos, yPos, 0f);
+                // Scaling.
+                asteroidObj.transform.localScale = new Vector3(scale, scale, 1);
+                asteroidAngularSpeed[xp, a] = Mathf.Pow(-1, a) * Random.Range(25f, 40f);
+            }
+        }
+        asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
     }
 
     // Update is called once per frame
     void Update()
     {
         // Updating the asteroids position.
-        //for (int i = 0; i < asteroids.Length; ++i)
-        //{
-        //    GameObject ast = asteroids[i];
 
-        //    int curLayer = (int)Mathf.Floor(i / (int)(asteroidsNum + 1));
-        //    int curAsteroidIdx = i % (int)(asteroidsNum + 1);
-        //    float curAsteroidSpeed = asteroidsLinearSpeed[curLayer];
-        //    float directionUp = curAsteroidSpeed / Mathf.Abs(curAsteroidSpeed);
+        if (Input.GetKeyDown("backspace"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
 
-        //    // Posiotion.
-        //    float x = ast.transform.position.x;
-        //    float y = ast.transform.position.y;
-        //    float z = ast.transform.position.z;
-        //    // Rotation.
-        //    float xr = ast.transform.localEulerAngles.x;
-        //    float yr = ast.transform.localEulerAngles.y;
-        //    float zr = ast.transform.localEulerAngles.z;
-        //    if (Mathf.Abs(y) > -initialYpos) // If the asteroid left the screen it is reposisioned at the opposite side.
-        //    {
-        //        y = directionUp * initialYpos;
-        //    }
-        //    else
-        //    {
-        //        y += curAsteroidSpeed * Time.deltaTime;
-        //        zr += asteroidAngularSpeed[curLayer, curAsteroidIdx] * Time.deltaTime;
-        //    }
-        //    ast.transform.position = new Vector3(x, y, z);
-        //    ast.transform.localEulerAngles = new Vector3(xr, yr, zr);
-        //}
+        for (int i = 0; i < asteroids.Length; ++i)
+        {
+            GameObject ast = asteroids[i];
 
-        //if (Input.GetKeyDown("backspace"))
-        //{
-        //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        //}
+            int curLayer = (int)Mathf.Floor(i / (int)(asteroidsNum + 1));
+            int curAsteroidIdx = i % (int)(asteroidsNum + 1);
+            float curAsteroidSpeed = asteroidsLinearSpeed[curLayer];
+            float directionUp = curAsteroidSpeed / Mathf.Abs(curAsteroidSpeed);
+
+            // Posiotion.
+            float x = ast.transform.position.x;
+            float y = ast.transform.position.y;
+            float z = ast.transform.position.z;
+            // Rotation.
+            float xr = ast.transform.localEulerAngles.x;
+            float yr = ast.transform.localEulerAngles.y;
+            float zr = ast.transform.localEulerAngles.z;
+            if (Mathf.Abs(y) > -initialYpos) // If the asteroid left the screen it is reposisioned at the opposite side.
+            {
+                y = directionUp * initialYpos;
+            }
+            else
+            {
+                y += curAsteroidSpeed * Time.deltaTime;
+                zr += asteroidAngularSpeed[curLayer, curAsteroidIdx] * Time.deltaTime;
+            }
+            ast.transform.position = new Vector3(x, y, z);
+            ast.transform.localEulerAngles = new Vector3(xr, yr, zr);
+        }
+
+        if (Input.GetKeyDown("backspace"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
     }
 }
